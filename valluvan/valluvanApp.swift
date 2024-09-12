5f1e6164146c35c42d8f0d4f8666ce0343cea69d
Devaraj <nsdevaraj@gmail.com>
2024-09-12 11:57:12 +0530

    Initial Commit

---
+++ valluvan/valluvanApp.swift
@@ -0,0 +1,17 @@
+//
+//  valluvanApp.swift
+//  valluvan
+//
+//  Created by DevarajNS on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct valluvanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
