9bbdd95fb4381b7b74e385d0686de3bb429e7ebc
Devaraj <nsdevaraj@gmail.com>
2024-09-15 10:28:45 +0530

    fixes

--- valluvan/ContentView.swift
+++ valluvan/ContentView.swift
@@ -15,39 +15,6 @@ struct Chapter: Identifiable {
     let audioPath: String
 }
 
-func getSystemImageForIyal(_ iyal: String) -> String {
-    switch iyal {
-    case "Preface", "பாயிரவியல்":
-        return "book.fill"
-    case "Domestic Virtue", "இல்லறவியல்":
-        return "house.fill"
-    case "Ascetic Virtue", "துறவறவியல்":
-        return "leaf.fill"
-    case "Royalty", "அரசியல்":
-        return "crown.fill"
-    case "Ministry", "அமைச்சியல்":
-        return "briefcase.fill"
-    case "Politics", "அரணியல்":
-        return "building.columns.fill"
-    case "Friendship", "நட்பியல்":
-        return "person.2.fill"
-    case "Miscellaneous":
-        return "square.grid.2x2.fill"
-    case "கூழியல்", "pulp":
-        return "cup.and.saucer.fill"
-    case "படையில்", "army":
-        return "shield.fill"
-    case "குடியியல்", "civility":
-        return "person.3.fill"
-    case "Pre-marital love", "களவியல்":
-        return "heart.fill"
-    case "Post-marital love", "கற்பியல்":
-        return "heart.circle.fill"
-    default:
-        return "circle.fill"
-    }
-}
-
 struct ContentView: View {
     @State private var selectedPal: String
     @State private var iyals: [String] = []
@@ -215,7 +182,7 @@ struct ContentView: View {
 
         var body: some View {
             HStack {
-                Image(systemName: getSystemImageForIyal(iyal))
+                Image(systemName: IyalUtils.getSystemImageForIyal(iyal))
                     .foregroundColor(.yellow)
                     .padding(.trailing, 8)
                 VStack(alignment: .leading, spacing: 8) {
